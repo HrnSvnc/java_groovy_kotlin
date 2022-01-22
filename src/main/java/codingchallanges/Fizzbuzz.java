62cc65cf1cf1947188625e937cf451d2bfdda031
Harun Sevinc <harun.sevinc@adesso.de>
2022-01-22 22:26:11 +0100

    Java example made functional

--- src/main/java/codingchallanges/Fizzbuzz.java
+++ src/main/java/codingchallanges/Fizzbuzz.java
@@ -1,11 +1,17 @@
 package codingchallanges;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Fizzbuzz {
 
   public void execute(){
-    for(int i = 0; i<100;i++){
-      System.out.println(i%15==0?"FizzBuzz":i%3==0?"Fizz":i%5==0?"Buzz":i);
-    }
+    System.out.println(
+        IntStream
+            .range(1,100)
+            .mapToObj(String::valueOf)
+            .map(i->Integer.parseInt(i)%15==0?"FizzBuzz":Integer.parseInt(i)%3==0?"Fizz":Integer.parseInt(i)%5==0?"Buzz": i)
+            .collect(Collectors.toList()));
   }
 
   public static void main(String[] args){
