fdcc7b24f96a677cfc07bf6427e307c1abdc78e5
Harun Sevinc <harun.sevinc@adesso.de>
2022-01-22 22:56:22 +0100

    prints 100 numbers now

--- src/main/java/codingchallanges/Fizzbuzz.java
+++ src/main/java/codingchallanges/Fizzbuzz.java
@@ -8,7 +8,7 @@ public class Fizzbuzz {
   public void execute(){
     System.out.println(
         IntStream
-            .range(1,100)
+            .range(1,101)
             .mapToObj(String::valueOf)
             .map(i->Integer.parseInt(i)%15==0?"FizzBuzz":Integer.parseInt(i)%3==0?"Fizz":Integer.parseInt(i)%5==0?"Buzz": i)
             .collect(Collectors.toList()));
